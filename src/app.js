db5ed640496d743cef462b304ff23106f3b487ce
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2022-03-14 21:43:25 -0400

    fix: adding port

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000
+
 app.get('/', (request, response, next) => {
     response.send("<h1>Server is running on Deploy 🚀<h1>");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is running on port 3000 🚀`);
 });
