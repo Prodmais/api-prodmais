4ec2fe82bafbc44485817d66a43f887d3d17e857
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2022-03-27 15:10:54 -0400

    feat: adding configurations database sequelize

---
+++ src/database/migrations/00002-create-task.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Tasks');
+  }
+};
