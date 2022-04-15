e37cfe5e5c8e1e381696e0d2ee180ecf339e386d
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2022-04-15 18:19:09 -0400

    fix: controllers tasks

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,82 +1,82 @@
 const { TaskService } = require("../services");
-const {AppError} = require('../errors');
+const { AppError } = require('../errors');
 const { TaskErrors } = require("../errors/messages");
 
 module.exports = {
-    create: async function(request, response) {
-        const { name, description, status } = request.body;
-        const userId = request.userId;
+  create: async function (request, response) {
+    const { name, description, status } = request.body;
+    const userId = request.userId;
 
-        const data = {
-            userId,
-            name,
-            description,
-            status,
-        }
+    const data = {
+      userId,
+      name,
+      description,
+      status,
+    }
 
-        const task = await TaskService.create(data);
+    const task = await TaskService.create(data);
 
-        response.status(201).json(task);
-    },
+    response.status(201).json(task);
+  },
 
-    update: async function(request, response) {
-      const { id } = request.params;
-      const userId = request.userId; 
+  update: async function (request, response) {
+    const { id } = request.params;
+    const userId = request.userId;
 
-      const { name, description, status } = request.body;
+    const { name, description, status } = request.body;
 
-      const taskExists = await TaskService.findById(id, userId);
+    const taskExists = await TaskService.findById(id, userId);
 
-      if(!taskExists){
-        throw new AppError(TaskErrors.TASK001);
-      }
+    if (!taskExists) {
+      throw new AppError(TaskErrors.TASK001);
+    }
 
-      const data = {
-        userId,
-        id,
-        name,
-        description,
-        status,
-      }
+    const data = {
+      id,
+      name,
+      description,
+      status,
+      userId,
+    }
 
-      const task = await TaskService.update(data);
+    const task = await TaskService.update(id, data);
 
-      response.status(200).json(task);
-    },
+    response.status(200).json(task);
+  },
 
-    delete: async function(request, response) {
-      const { id } = request.params;
-      const userId = request.userId; 
+  delete: async function (request, response) {
+    const { id } = request.params;
+    const userId = request.userId;
 
-      const taskExists = await TaskService.findById(id, userId);
+    const taskExists = await TaskService.findById(id, userId);
 
-      if(!taskExists){
-        throw new AppError(TaskErrors.TASK001);
-      }
+    if (!taskExists) {
+      throw new AppError(TaskErrors.TASK001);
+    }
 
-      const task = await TaskService.delete(id, userId);
+    const task = await TaskService.delete(id, userId);
 
-      response.status(200).json(task);
-    },
+    response.status(200).json(task);
+  },
 
-    findAll: async function(request, response) {
-      const userId = request.userId;
+  findAll: async function (request, response) {
+    const userId = request.userId;
 
-      const tasks = await TaskService.findAll(userId);
+    const tasks = await TaskService.findAll(userId);
 
-      response.status(200).json(tasks);
-    },
+    response.status(200).json(tasks);
+  },
 
-    findOne: async function (request, response) {
-      const id = request.params
-      const userId = request.userId
+  findOne: async function (request, response) {
+    const { id } = request.params
+    const userId = request.userId
 
-      const ExistTask = await TaskService.findById(id, userId);
+    const existTask = await TaskService.findById(id, userId);
 
-      if(!ExistTask) {
-        throw new AppError(TaskErrors.TASK001);
-      }
-  
-      response.status(200).json(task);
-    },
+    if (!existTask) {
+      throw new AppError(TaskErrors.TASK001);
+    }
+
+    response.status(200).json(existTask);
+  },
 }
